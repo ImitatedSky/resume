7d8c126a1cc7136f576d802b06372c8e3b537c1b
PC,Y <111834126+ImitatedSky@users.noreply.github.com>
2023-05-09 06:00:03 +0000

    Update App.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import "./styles.css";
+import { BrowserRouter, Route, Switch, NavLink } from "react-router-dom";
+
+import Header from "./components/header";
+import Footer from "./components/footer";
+
+import Home from "./components/Home/Home";
+import AboutMe from "./components/AboutMe/AboutMe";
+import Contact from "./components/Contact/Contact";
+import Experience from "./components/Experience/Experience";
+import Project from "./components/Project/Project";
+import Nopage from "./components/Nopage/Nopage";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/AboutMe">
+          <AboutMe />
+        </Route>
+        <Route path="/Experience">
+          <Experience />
+        </Route>
+        <Route path="/Project">
+          <Project />
+        </Route>
+        <Route path="/Contact">
+          <Contact />
+        </Route>
+      </Switch>
+      <Footer />
+    </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,15 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Switch, NavLink } from "react-router-dom";
 
-import Header from "./components/header";
-import Footer from "./components/footer";
-
-import Home from "./components/Home/Home";
-import AboutMe from "./components/AboutMe/AboutMe";
-import Contact from "./components/Contact/Contact";
-import Experience from "./components/Experience/Experience";
-import Project from "./components/Project/Project";
-import Nopage from "./components/Nopage/Nopage";
+import App from "./App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -18,25 +10,7 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <BrowserRouter>
-      <Header />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/AboutMe">
-          <AboutMe />
-        </Route>
-        <Route path="/Experience">
-          <Experience />
-        </Route>
-        <Route path="/Project">
-          <Project />
-        </Route>
-        <Route path="/Contact">
-          <Contact />
-        </Route>
-      </Switch>
-      <Footer />
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
